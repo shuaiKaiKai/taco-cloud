e4daaa7261bf1477669f39e10e1984aada81194d
shuaiKaiKai <530909148@qq.com>
2021-11-12 19:07:10 +0800

    提交

---
+++ src/main/java/tacos/model/Taco.java
@@ -0,0 +1,26 @@
+package tacos.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 煎饼
+ */
+@Data
+public class Taco {
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<String> ingredients;
+
+    private Date createdAt;
+}
